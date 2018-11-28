288d21f1b3ce40d7a83170c70f736e873a02997d
vanessanaja <vanessacnaja@gmail.com>
2018-11-28 11:26:23 -0800

    added generateRandomColors and  randomColor functions

--- ColorGame.js
+++ ColorGame.js
@@ -1,11 +1,4 @@
-let colors = [
-    'rgb(255, 0, 0)', 
-    'rgb(255, 255, 0)', 
-    'rgb(0, 255, 0)', 
-    'rgb(0, 255, 255)', 
-    'rgb(0, 0, 255)', 
-    'rgb(255, 0, 255)',   
-]
+let colors = generateRandomColors(6);
 const squares = document.querySelectorAll('.square');
 let pickedColor = pickColor();
 const colorDisplay = document.getElementById('colorDisplay');
@@ -36,4 +29,17 @@ function changeColor(color){
 function pickColor(){
     let random = Math.floor(Math.random() * colors.length);
     return colors[random];
+}
+function generateRandomColors(num){
+    let arr = [];
+    for(let i = 0; i < num; i++){
+        arr.push(randomColor());
+    }
+    return arr;
+}
+function randomColor(){
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+    return 'rgb('+ r + ', ' + g + ', ' + b +')';
 }
