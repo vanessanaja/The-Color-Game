f5b4665bde04d6b26575575e2cdb4e5b09fe91d0
vanessanaja <vanessacnaja@gmail.com>
2018-11-28 10:35:31 -0800

    added logic v1
    ability to pick squares,
    alert if correct color was picked or not,
    change color of square to black for wrong guesses,
    change color of all squares to picked color for correct guesses
    changed variables to reflect ES6

--- ColorGame.js
+++ ColorGame.js
@@ -1,4 +1,4 @@
-var colors = [
+let colors = [
     'rgb(255, 0, 0)', 
     'rgb(255, 255, 0)', 
     'rgb(0, 255, 0)', 
@@ -6,8 +6,30 @@ var colors = [
     'rgb(0, 0, 255)', 
     'rgb(255, 0, 255)',   
 ]
-var squares = document.querySelectorAll('.square');
+const squares = document.querySelectorAll('.square');
+let pickedColor = colors[3];
+const colorDisplay = document.getElementById('colorDisplay');
+colorDisplay.textContent = pickedColor;
+const messageDisplay = document.querySelector('#message');
 
-for(var i =0; i < squares.length; i++){
+for(let i =0; i < squares.length; i++){
+    //add initial color to squares
     squares[i].style.backgroundColor = colors[i]; 
+    squares[i].addEventListener('click', function(){
+        //grab color of clicked square
+        let clickedColor = this.style.backgroundColor;
+            if(clickedColor === pickedColor) {
+                messageDisplay.textContent = 'Correct!';
+                changeColor();
+            } else {
+                this.style.backgroundColor = '#232323';
+                messageDisplay.textContent = 'Try Again';
+            }
+    });
+}
+
+function changeColor(color){
+    for(let i = 0; i < squares.length; i++){
+        squares[i].style.backgroundColor = pickedColor;
+    }
 }
