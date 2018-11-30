55e079a021f0d5f4d91a0e15342c39144d32c09e
vanessanaja <vanessacnaja@gmail.com>
2018-11-29 20:43:30 -0800

    change event handler

--- ColorGame.js
+++ ColorGame.js
@@ -2,7 +2,7 @@ let numSquares = 6;
 let colors = [];
 let pickedColor;
 const squares = document.querySelectorAll('.square');
-const colorDisplay = document.getElementById('colorDisplay');
+const colorDisplay = document.querySelector('#colorDisplay');
 const messageDisplay = document.querySelector('#message');
 const h1 = document.querySelector('h1');
 const resetButton = document.querySelector('#reset');
