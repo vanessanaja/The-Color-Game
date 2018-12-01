cef5ffc5afa15b59264b71cfc74acfe83fcdf079
vanessanaja <vanessacnaja@gmail.com>
2018-12-01 11:37:40 -0800

    Refactor as Object Oriented initial commit

---
+++ CGRefactorOO.js
@@ -0,0 +1,135 @@
+let color = {
+    let colors = [],
+    let pickedColor,
+    pickColor: function() {
+        let random = Math.floor(Math.random() * colors.length);
+        return colors[random];
+    },
+    generateRandomColors: function(num) {
+        let arr = [];
+        for (let i = 0; i < num; i++) {
+            arr.push(randomColor());
+        }
+        return arr;
+    },
+    randomColor: function() {
+        let r = Math.floor(Math.random() * 256);
+        let g = Math.floor(Math.random() * 256);
+        let b = Math.floor(Math.random() * 256);
+        return 'rgb(' + r + ', ' + g + ', ' + b + ')';
+    }
+
+};
+
+let handlers = {
+    const resetButton = document.querySelector('#reset');
+    const modeButtons = document.querySelectorAll('.mode');
+
+};
+
+let view = {
+    let numSquares = 6;
+    const colorDisplay = document.querySelector('#colorDisplay'),
+    const messageDisplay = document.querySelector('#message'),
+    const h1 = document.querySelector('h1'),
+    const squares = document.querySelectorAll('.square');
+
+};
+
+
+// let numSquares = 6;
+// let colors = [];
+// let pickedColor;
+// const squares = document.querySelectorAll('.square');
+// const colorDisplay = document.querySelector('#colorDisplay');
+// const messageDisplay = document.querySelector('#message');
+// const h1 = document.querySelector('h1');
+// const resetButton = document.querySelector('#reset');
+// const modeButtons = document.querySelectorAll('.mode');
+
+init();
+
+function init() {
+    setupModeButtons();
+    setupSquares();
+    reset();
+}
+
+function setupModeButtons() {
+    //mode button event listeners
+    for (let i = 0; i < modeButtons.length; i++) {
+        modeButtons[i].addEventListener('click', function () {
+            modeButtons[0].classList.remove('selected');
+            modeButtons[1].classList.remove('selected');
+            this.classList.add('selected');
+            this.textContent === 'EASY' ? numSquares = 3 : numSquares = 6;
+            reset();
+        });
+    }
+}
+
+function setupSquares() {
+    for (let i = 0; i < squares.length; i++) {
+        squares[i].addEventListener('click', function () {
+            //grab color of clicked square
+            let clickedColor = this.style.backgroundColor;
+            if (clickedColor === pickedColor) {
+                messageDisplay.textContent = 'Correct!';
+                changeColor(clickedColor);
+                h1.style.backgroundColor = pickedColor;
+                resetButton.textContent = "Play Again?";
+            } else {
+                this.style.backgroundColor = '#232323';
+                messageDisplay.textContent = 'Try Again';
+                // resetButton.textContent = "New Colors";
+            }
+        });
+    }
+}
+
+function reset() {
+    colors = generateRandomColors(numSquares);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+    resetButton.textContent = "New Colors";
+    messageDisplay.textContent = '';
+    for (let i = 0; i < squares.length; i++) {
+        if (colors[i]) {
+            squares[i].style.display = 'block';
+            squares[i].style.backgroundColor = colors[i];
+        } else {
+            squares[i].style.display = 'none';
+        }
+    }
+    h1.style.backgroundColor = 'steelblue';
+}
+
+resetButton.addEventListener('click', function () {
+    reset();
+});
+
+function changeColor(color) {
+    for (let i = 0; i < squares.length; i++) {
+        squares[i].style.backgroundColor = pickedColor;
+    }
+}
+
+// function pickColor() {
+//     let random = Math.floor(Math.random() * colors.length);
+//     return colors[random];
+// }
+
+// function generateRandomColors(num) {
+//     let arr = [];
+//     for (let i = 0; i < num; i++) {
+//         arr.push(randomColor());
+//     }
+//     return arr;
+// }
+
+// function randomColor() {
+//     let r = Math.floor(Math.random() * 256);
+//     let g = Math.floor(Math.random() * 256);
+//     let b = Math.floor(Math.random() * 256);
+//     return 'rgb(' + r + ', ' + g + ', ' + b + ')';
+// }
